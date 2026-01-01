06cb7036c169302ab15efeebd0c194caee2589ea
RhodyArthur <rhodaarthur48@gmail.com>
2026-01-01 19:16:58 +0000

    feat: add variable creation and type printing example

---
+++ variables_data_types_operators.py
@@ -0,0 +1,15 @@
+# Task 1.1
+# Create variables for: an integer, float, string, boolean, and None type.
+# Print each with its type using `type()`.
+
+age = 24
+price = 23.99
+name = "Esi Gyeyimah"
+isExisting = True
+value = None
+
+print(f"Integer value: {age}, datatype: {type(age)}")
+print(f"Float value: {price}, datatype: {type(price)}")
+print(f"String value: {name}, datatype: {type(name)}")
+print(f"Boolean value: {isExisting}, datatype: {type(isExisting)}")
+print(f"None value: {value}, datatype: {type(value)}")
